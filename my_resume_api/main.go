d2ce12fc33476c2c640694ada12739ca9a60235f
Kevin Lux <61444549+kevinlux@users.noreply.github.com>
2022-01-31 17:34:08 +1300

    update main.go, go.sum, go.mod

--- my_resume_api/main.go
+++ my_resume_api/main.go
@@ -4,15 +4,20 @@ import (
 	"github.com/gin-gonic/gin"
 	"jsonresume/configs"
 	"jsonresume/routes"
+	"log"
 	"os"
 )
 
 func main() {
-	r := gin.Default()
-	r.Static("/css", "./static/css")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	r := gin.New()
+	r.Use(gin.Logger())
 	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/css", "./static/css")
 	configs.ConnectDB()
 	routes.ResumeRoute(r)
-	r.Run("localhost:8000")
-
+	r.Run(":" + port)
 }
